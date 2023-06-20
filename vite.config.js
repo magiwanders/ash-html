7a1cf7c47f85df3bcc2e4976c39be69afe375f4f
magiwanders <simone.shawn.cazzaniga@gmail.com>
2023-06-20 11:55:47 +0200

    added npm workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.js'),
-      name: 'ash-js',
+      name: '@magiwanders/ash-js',
       fileName: 'ash-js',
     },
   },
